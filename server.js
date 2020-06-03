6e1a7e067dfd99cc7438675110145808b411a0c6
Viti <vge@perama.es>
2020-06-03 15:03:33 +0200

    change dir path

--- server.js
+++ server.js
@@ -2,12 +2,13 @@ var express = require('express');
 var app = express();
 var server = require('http').Server(app);
 var bodyParser = require('body-parser');
+const path = require('path');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(express.static(__dirname + '/dist/cypto-live'));
-
+app.use(express.static(__dirname + '/dist/crypto-live'));
+console.log('dirname', __dirname)
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname + '/dist/cypto-live/index.html'));
 });
